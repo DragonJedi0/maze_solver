d318f2b9dd1e30d191f664f7589f274ae288c9d3
Samuel Ybarra <sybarra2076@gmail.com>
2025-09-03 13:32:17 -0500

    Adding drawing animation

--- main.py
+++ main.py
@@ -4,7 +4,21 @@ from maze import Maze
 def main():
     win = Window(800, 600)
 
-    maze = Maze(0,0,6,8,100,100,win)
+    start_x = 0
+    start_y = 0
+    maze_width = 12
+    maze_height = 16
+    cell_width = 50
+    cell_height = 50
+
+    Maze(start_x,
+         start_y,
+         maze_width,
+         maze_height,
+         cell_width,
+         cell_height,
+         win
+        )
 
     win.wait_for_close()
 
